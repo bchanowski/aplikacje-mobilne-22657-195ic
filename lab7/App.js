320b9fd63322e98ae73a1f5ba8dab0115ff408e2
bchanowski <bartosz.chanowski@gmail.com>
2022-06-14 23:31:56 +0200

    HomePage

--- lab7/App.js
+++ lab7/App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import * as React from "react";
+import { Button, View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./pages/HomeScreen";
+import RegisterUser from "./pages/RegisterUser";
+import UpdateUser from "./pages/UpdateUser";
+import ViewUser from "./pages/ViewUser";
+import ViewAllUser from "./pages/ViewAllUser";
+import DeleteUser from "./pages/DeleteUser";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRoute="HomeScreen">
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="ViewUser" component={ViewUser} />
+        <Stack.Screen name="ViewAllUser" component={ViewAllUser} />
+        <Stack.Screen name="UpdateUser" component={UpdateUser} />
+        <Stack.Screen name="RegisterUser" component={RegisterUser} />
+        <Stack.Screen name="DeleteUser" component={DeleteUser} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
